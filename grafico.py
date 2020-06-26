97f712425f2b7f26585d39c0fd2a03393cc09e9b
rodrigonevest <56009318+rodrigonevest@users.noreply.github.com>
2020-06-25 23:03:42 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,6 +1,4 @@
-import pandas as pd
-import seaborn as sb
-import csv
+
 import matplotlib.pyplot as plt
 
 dados = open('CORONA1.csv').readlines()
@@ -13,16 +11,8 @@ for i in range(len(dados)):
         linha = dados[i].split(';')
         x.append(str(linha[1]))
         y.append(str(linha[0]))
-        #z.append(str(linha[2]))
-
-
-#plt.bar(x,y, color='#e4e4e4')
-#plt.plot(x,y, color='k', linestyle='--')
+        
 plt.stem(y,x)
-#plt.hist(y)
-#plt.step(y,x)
-#plt.errorbar(x,y)
-#plt.barh(y,x)
 
 plt.title('Faixa etária de pessoas atingida pelo COVID-19 na região proximo a Frutal-MG ate 24/06/2020\nFonte:"http://coronavirus.saude.mg.gov.br/painel"')
 plt.suptitle('COVID-19')
